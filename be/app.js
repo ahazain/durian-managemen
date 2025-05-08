5d2b233d909e5f179a3b32602fc7ac954718874b
ahazain <ahmadfariddd5@gmail.com>
2025-05-09 06:51:28 +0700

    add fitur profil: getProfil & updateProfil

--- be/app.js
+++ be/app.js
@@ -2,15 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const autentifikasiRoute = require("./src/routes/autentifikasi-route")
-const profilRoute = require("./src/routes/profil-route")
+const autentifikasiRoute = require("./src/routes/autentifikasi-route");
+const profilRoute = require("./src/routes/profil-route");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/autentifikasi", autentifikasiRoute);
-app.unsubscribe("/api/v1", profilRoute)
+app.use("/api/v1", profilRoute),
+app.use((error, req, res, next) => {
+  console.log(error.stack);
+  res.status(error.status || 500).send({
+    error: {
+      message: error.message || "Internal server error",
+    },
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
